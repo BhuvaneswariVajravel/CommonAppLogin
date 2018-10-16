5596c12f6b1517c11172e237900bca85aabe3bc5
bhuvi <bhuvanapiya@gmail.com>
2018-10-16 17:20:46 +0530

    Update src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { appRouting } from './app.routing';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    //appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
